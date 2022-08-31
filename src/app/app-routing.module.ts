580b925d73b114c2432f273628279a8e26c6d38c
Gagan Gaurav <iamgagangaurav@gmail.com>
2022-09-01 00:43:11 +0530

    changed background

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WallComponent } from 'src/lib/wall/wall.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/app-wall', pathMatch : 'full'},
+  { path: "app-wall", component: WallComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
